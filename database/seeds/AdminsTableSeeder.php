e0283efd4bbca65394e701acb84cf62ef70f6a91
r_sato <0tx03v11788390d@gmail.com>
2021-02-08 15:43:35 +0900

    adminシードにユーザーをもう一人追加

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -14,11 +14,20 @@ class AdminsTableSeeder extends Seeder
         $table = DB::table('admins');
         $table->truncate();
         $table->insert([
-            'name' => '田中 太郎',
-            'email' => 'tanaka@example.com',
-            'password' => bcrypt('12345678'),
-            'created_at' => '2021-01-01 10:00:00',
-            'updated_at' => '2021-01-01 10:00:00'
+            [
+                'name' => '田中 太郎',
+                'email' => 'tanaka@example.com',
+                'password' => bcrypt('12345678'),
+                'created_at' => '2021-01-01 10:00:00',
+                'updated_at' => '2021-01-01 10:00:00'
+            ],
+            [
+                'name' => '山田 次郎',
+                'email' => 'yamada@example.com',
+                'password' => bcrypt('12345678'),
+                'created_at' => '2021-02-01 10:00:00',
+                'updated_at' => '2021-02-01 10:00:00'
+            ]
         ]);
     }
 }
